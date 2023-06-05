d30341637085731d509ae270cc92872250cf4603
32rfs <32rafael.ss@gmail.com>
2023-06-05 16:55:21 +0100

    Push

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
-import tensorflow as tf
 from flask import Flask, render_template, request, jsonify
 import numpy as np
-
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 import urllib.request
 import bs4 as bs
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-
-data = pd.read_csv('./Dataset/processed_dataset/main_data.csv')
+data = pd.read_csv('Dataset/processed_dataset/old_main_data.csv')
 vectorizer = pickle.load(open('tranformVector.pkl', 'rb'))
-model = pickle.load(open('nb_model.pkl', 'rb'))
-
-def create_similarity():
-    # creating a count matrix
-    cv = CountVectorizer()
-    count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
-    similarity = cosine_similarity(count_matrix)
-    return data,similarity
+model = pickle.load(open('svm_model.pkl', 'rb'))
 
 
-def recomend_movie(m):
-    m = m.lower()
+# Create Similarity Matrix using CountVectorizer and Cosine Similarity
+def createSimilarity():
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data['comb'])
+    cosine_similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+    return data, cosine_similarity
+
+# function that takes in movie title as input and returns the top 10 recommended movies
+def recomendation(movie):
+    movie = movie.lower()
     try:
         data.head()
         similarity.shape
     except:
-        data, similarity = create_similarity()
-    if m not in data['movie_title'].unique():
+        data, similarity = createSimilarity()
+    if movie not in data['movie_title'].unique():
         return (
             'Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies')
     else:
-        i = data.loc[data['movie_title'] == m].index[0]
+        i = data.loc[data['movie_title'] == movie].index[0]
         lst = list(enumerate(similarity[i]))
         lst = sorted(lst, key=lambda x: x[1], reverse=True)
         lst = lst[1:11]  # excluding first item since it is the requested movie itself
@@ -44,27 +41,29 @@ def recomend_movie(m):
             l.append(data['movie_title'][a])
         return l
 
-# converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
-def convert_to_list(my_list):
+# function to convert string to list
+def converToList(my_list):
     my_list = my_list.split('","')
     my_list[0] = my_list[0].replace('["','')
     my_list[-1] = my_list[-1].replace('"]','')
     return my_list
 
-def get_suggestions():
-    data = pd.read_csv('./Dataset/processed_dataset/main_data.csv')
+# function to get movie suggestions for auto complete
+def getSuggestions():
     return list(data['movie_title'].str.capitalize())
 
+
+# Flask app
 app = Flask(__name__)
 
-
-
+# Home page route
 @app.route("/")
 @app.route("/home")
 def home():
-    suggestions = get_suggestions()
+    suggestions = getSuggestions()
     return render_template('home.html', suggestions=suggestions)
 
+# Auto complete route
 @app.route('/autocomplete')
 def autocomplete():
     search = request.args.get('search')
@@ -72,26 +71,27 @@ def autocomplete():
     results = [movie for movie in suggestions if search.lower() in movie.lower()]
     return jsonify(results)
 
-
+# Similarity route and function
 @app.route("/similarity", methods=["POST"])
 def similarity():
     movie = request.form['name']
-    rc = recomend_movie(movie)
+    rc = recomendation(movie)
     if type(rc) == type('string'):
         return rc
     else:
         m_str = "---".join(rc)
         return m_str
 
+# Chatbot page route
 @app.route("/chatbot")
 def chatbot():
-    suggestions = get_suggestions()
+    suggestions = getSuggestions()
     return render_template('chatbot.html', suggestions=suggestions)
 
-
+# Recommendation page route and function
 @app.route("/recommend", methods=["POST"])
 def recommend():
-    # getting data from AJAX request
+    # Using Ajax to get the movie metadada from the webpage
     title = request.form['title']
     cast_ids = request.form['cast_ids']
     cast_names = request.form['cast_names']
@@ -104,37 +104,35 @@ def recommend():
     poster = request.form['poster']
     genres = request.form['genres']
     overview = request.form['overview']
-    vote_average = request.form['rating']
-    vote_count = request.form['vote_count']
     release_date = request.form['release_date']
     runtime = request.form['runtime']
     status = request.form['status']
     rec_movies = request.form['rec_movies']
     rec_posters = request.form['rec_posters']
 
-    # get movie suggestions for auto complete
-    suggestions = get_suggestions()
+    # Getting movie suggestions
+    suggestions = getSuggestions()
 
-    # call the convert_to_list function for every string that needs to be converted to list
-    rec_movies = convert_to_list(rec_movies)
-    rec_posters = convert_to_list(rec_posters)
-    cast_names = convert_to_list(cast_names)
-    cast_chars = convert_to_list(cast_chars)
-    cast_profiles = convert_to_list(cast_profiles)
-    cast_bdays = convert_to_list(cast_bdays)
-    cast_bios = convert_to_list(cast_bios)
-    cast_places = convert_to_list(cast_places)
+    # Convert string to list (eg. "[1,2,3]" to [1,2,3])
+    rec_movies = converToList(rec_movies)
+    rec_posters = converToList(rec_posters)
+    cast_names = converToList(cast_names)
+    cast_chars = converToList(cast_chars)
+    cast_profiles = converToList(cast_profiles)
+    cast_bdays = converToList(cast_bdays)
+    cast_bios = converToList(cast_bios)
+    cast_places = converToList(cast_places)
 
-    # convert string to list (eg. "[1,2,3]" to [1,2,3])
+    # Splitting the string to list
     cast_ids = cast_ids.split(',')
     cast_ids[0] = cast_ids[0].replace("[", "")
     cast_ids[-1] = cast_ids[-1].replace("]", "")
 
-    # rendering the string to python string
+    # Replacing the escape characters
     for i in range(len(cast_bios)):
         cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"', '\"')
 
-    # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
+    # Creating dictionaries
     movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
 
     casts = {cast_names[i]: [cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
@@ -142,35 +140,35 @@ def recommend():
     cast_details = {cast_names[i]: [cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in
                     range(len(cast_places))}
 
-    # web scraping to get user reviews from IMDB site
-
+    # Getting the reviews using WebScraping
     with urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)) as url:
         sauce = url.read()
     #sauce = request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
+    # Parsing the html
     soup = bs.BeautifulSoup(sauce, 'lxml')
     soup_result = soup.find_all("div", {"class": "text show-more__control"})
 
-    reviews_list = []  # list of reviews
-    reviews_status = []  # list of comments (good or bad)
+    reviews_list = []  #List of reviews
+    reviews_status = []  #List of reviews status
 
+    # Getting the reviews
     for reviews in soup_result:
         if reviews.string:
             reviews_list.append(reviews.string)
-            # passing the review to our model
+            # Predicting the review status
             movie_review_list = np.array([reviews.string])
             movie_vector = vectorizer.transform(movie_review_list)
             pred = model.predict(movie_vector)
             reviews_status.append('Good' if pred else 'Bad')
 
-    # combining reviews and comments into a dictionary
+    # Creating a dictionary with the movie reviews and their status
     movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}
 
-    # passing all the data to the html file
-    return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
-                           vote_count=vote_count, release_date=release_date, runtime=runtime, status=status,
+    # Rendering the recommendation page
+    return render_template('recommend.html', title=title, poster=poster, overview=overview, release_date=release_date, runtime=runtime, status=status,
                            genres=genres,
                            movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details)
 
-
+# Running the app
 if __name__ == '__main__':
     app.run(debug=True)
